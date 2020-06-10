301ada52c165349bb586cec8bc8f3770ce694f40
Andras <aszucsak@gmail.com>
2020-06-10 22:18:54 +0200

    add homepage and styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import HomePage from './homepage.component';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello World</h1>
+        <HomePage />
       </div>
     );
   }
