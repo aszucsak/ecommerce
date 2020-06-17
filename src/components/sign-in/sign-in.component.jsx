d83bef226b4b0f20f910acd503caee9e83b9eb44
Andras <aszucsak@gmail.com>
2020-06-17 22:39:01 +0200

    start preparing firebase project

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
 
 import './sign-in.style.scss';
 
@@ -45,7 +46,7 @@ export default class SignIn extends Component {
             handleChange={this.handleChange}
             label="Password"
           />
-          <button type="submit">Submit Form</button>
+          <CustomButton type="submit"> Sign in </CustomButton>
         </form>
       </div>
     );
