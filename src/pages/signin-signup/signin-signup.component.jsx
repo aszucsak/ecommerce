533aeb1dd38d979cc03ee690dc9232c3ab486012
Andras <aszucsak@gmail.com>
2020-06-24 21:49:50 +0200

    implement firebase utils included ability to store authenticated users

--- src/pages/signin-signup/signin-signup.component.jsx
+++ src/pages/signin-signup/signin-signup.component.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 
 import Signin from '../../components/sign-in/sign-in.component';
 
+import SignUp from '../../components/sign-up/sign-up.component';
+
 import './signin-signup.styles.scss';
 
 const SigninSignupPage = () => {
   return (
     <div className="signin-signup">
       <Signin />
+      <SignUp />
     </div>
   );
 };
