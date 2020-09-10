1bc1298175e59b081c5e53aecbc1c5c405075dcf
András Szucsák <andras.szucsak@urss.hu>
2020-09-10 21:09:28 +0200

    separate server code and client code

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
@@ -7,8 +8,22 @@ const StripeCheckoutButton = ({ price }) => {
     'pk_test_51H9cSnHepWYFzX5vW02rxrYDIBzPMafBdlMtRIXhOd9Vts3L7j9d1yNdyPJg0g7o7x4UJT527rhUen2JbP5uQiby00EXSDMPEO';
 
   const onToken = (token) => {
-    console.log(token);
-    alert('Payment Successful');
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token: token,
+      },
+    })
+      .then((response) => {
+        alert('Payment successful');
+      })
+      .catch((error) => {
+        console.log('payment error: ');
+        console.log(error);
+        alert('There was an issue with your payment.');
+      });
   };
 
   return (
